PotatoProject/packages_apps_Settings

AmbientDisplay : Add Now Playing entry
Change-Id: I7fb8b30f1cbefe244435fabf783386d72a03a59a
Signed-off-by: Shreyansh Lodha <slodha96@gmail.com>

src/com/android/settings/core/gateway/SettingsGateway.java
@@ -16,6 +16,7 @@
 
 package com.android.settings.core.gateway;
 
+import com.android.settings.AmbientPlaySettings;
 import com.android.settings.DateTimeSettings;
 import com.android.settings.DeviceAdminSettings;
 import com.android.settings.DisplaySettings;
@@ -263,6 +264,7 @@ public class SettingsGateway {
             DirectoryAccessDetails.class.getName(),
             ToggleBackupSettingFragment.class.getName(),
             PreviouslyConnectedDeviceDashboardFragment.class.getName(),
+            AmbientPlaySettings.class.getName(),
     };
 
     public static final String[] SETTINGS_FOR_RESTRICTED = {

bigrushdog/android_packages_apps_Settings

src/com/android/settings/core/gateway/SettingsGateway.java
@@ -16,6 +16,7 @@
 
 package com.android.settings.core.gateway;
 
+import com.android.settings.AmbientPlaySettings;
 import com.android.settings.DateTimeSettings;
 import com.android.settings.DeviceAdminSettings;
 import com.android.settings.DisplaySettings;
@@ -273,7 +274,8 @@ public class SettingsGateway {
             NavigationSettingsActivity.class.getName(),
             SmartbarSettingsActivity.class.getName(),
             FlingSettingsActivity.class.getName(),
-            PulseSettingsActivity.class.getName()
+            PulseSettingsActivity.class.getName(),
+            AmbientPlaySettings.class.getName()
     };
 
     public static final String[] SETTINGS_FOR_RESTRICTED = {